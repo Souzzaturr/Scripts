76e3de5ddf2ebbe480bc6a03b607b35cfd1ae612
Arttur Souzza <souzzaaturr@gmail.com>
2025-10-07 22:53:54 -0300

    Adiciona classe 'EXAME'

---
+++ exam_class.js
@@ -0,0 +1,62 @@
+export class Exame {
+  constructor(resposta, peso) {
+      this.resposta = resposta;
+    this.pesos = peso;
+    this.provas = [];
+  }
+
+  adiciona(prova) {
+    prova.nota = Object.keys(prova.gabarito).reduce((total, q) => {
+      return (
+        total + (prova.gabarito[q] === this.resposta.gabarito[q] ? this.pesos[q] : 0)
+      );
+    }, 0);
+
+    this.provas.push(prova);
+  }
+
+  media_notas() {
+    let soma = Object.values(this.provas).reduce(
+      (soma, prova) => soma + prova.nota,
+      0
+    );
+
+    return soma / this.provas.length;
+  }
+
+  menor(count = 1) {
+    let lista_notas = this.provas.map((prova) => prova.nota);
+
+    lista_notas.sort((a, b) => a - b);
+
+    return lista_notas.slice(0, count);
+  }
+
+  maior(count = 1) {
+    let lista_notas = this.provas.map((prova) => prova.nota);
+
+    lista_notas.sort((a, b) => a - b);
+
+    return lista_notas.slice(-count);
+  }
+
+  menor_que(numero) {
+    let lista_notas = this.provas.map((prova) => prova.nota);
+
+    return lista_notas.filter((nota) => nota < numero);
+  }
+
+  maior_que(numero) {
+    let lista_notas = this.provas.map((prova) => prova.nota);
+
+    return lista_notas.filter((nota) => nota > numero);
+  }
+
+  lista_notas() {
+    let notas_list = [];
+
+    this.provas.forEach( (prova) => notas_list.push(prova.nota) );
+
+    return notas_list
+  }
+}
---
+++ exams_print.js
@@ -0,0 +1,44 @@
+import { Exame } from './exam_class.js';
+
+const pesos = { q1: 2, q2: 2, q3: 2, q4: 2, q5: 2 };
+
+const resposta = {
+  pessoa: 'Professor',
+  gabarito: { q1: 'a', q2: 'b', q3: 'a', q4: 'c', q5: 'd' },
+};
+
+const exame = new Exame(resposta, pesos);
+
+exame.adiciona({
+  aluno: 'Alice',
+  gabarito: { q1: 'a', q2: 'b', q3: 'b', q4: 'b', q5: 'b' },
+});
+
+console.log(`Media das notas: ${exame.media_notas()}`);
+
+console.log(`Menor nota: ${exame.menor()}`);
+
+console.log(`Maior nota: ${exame.maior()}`);
+
+console.log(`Notas abaixo de 7.0: ${exame.menor_que(7)}`);
+
+console.log(`Notas acima de 7.0: ${exame.maior_que(7)}`);
+
+console.log(`Lista de notas: ${exame.lista_notas()}`)
+
+exame.adiciona({
+  aluno: 'Bob',
+  gabarito: { q1: 'c', q2: 'b', q3: 'a', q4: 'c', q5: 'd' },
+});
+
+console.log(`Media das notas: ${exame.media_notas()}`);
+
+console.log(`Menor nota: ${exame.menor()}`);
+
+console.log(`Maior nota: ${exame.maior()}`);
+
+console.log(`Notas abaixo de 7.0: ${exame.menor_que(7)}`);
+
+console.log(`Notas acima de 7.0: ${exame.maior_que(7)}`);
+
+console.log(`Lista de notas: ${exame.lista_notas()}`)
